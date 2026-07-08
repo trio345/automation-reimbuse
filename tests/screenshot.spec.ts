00788a80bbe8854142d074a777d47f407e18aa7a
Trio Saputra <triosaputra308@gmail.com>
2026-07-08 14:44:49 +0700

    update date format

--- tests/screenshot.spec.ts
+++ tests/screenshot.spec.ts
@@ -1,11 +1,14 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+
 
 test('click dropdown and take screenshot', async ({ page }) => {
     // Ubah ukuran browser menjadi tampilan smartphone (320 x 568)
     await page.setViewportSize({ width: 320, height: 500 });
 
     // 1. Arahkan ke halaman web yang diinginkan
-    await page.goto('https://r.parkee.app/?l=164&ps=4BE0B9CF');
+    await page.goto('https://r.parkee.app/?l=164&ps=427592CF');
 
     // Tunggu loading overlay (MuiLinearProgress-root) hilang jika ada
     await page.waitForSelector('.MuiLinearProgress-root', { state: 'detached', timeout: 5000 }).catch(() => { });
@@ -19,8 +22,12 @@ test('click dropdown and take screenshot', async ({ page }) => {
     // Tunggu animasi dropdown selesai terbuka
     await page.waitForTimeout(500);
 
-    const date = new Date();
-    const dateString = date.toISOString().split('T')[0]; // Contoh output: "2026-05-10"
+    const elEntryDate = await page.locator('#receipt-entryTime').textContent();
+
+    dayjs.extend(customParseFormat);
+
+    const dateString = dayjs(elEntryDate, 'DD MMM YYYY, HH:mm').format('YYYY-MM-DD');
+
     const fileName = `screenshot-${dateString}.png`;
 
     await page.screenshot({ path: `./screenshoots/${fileName}`, fullPage: false });
